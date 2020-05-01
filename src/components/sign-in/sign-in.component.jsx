13d998d7bc6ef4e503e65493488c3f45320b000a
Lucas <lucasbezerra1701@gmail.com>
2020-05-01 10:05:44 -0300

    M06#09

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.component.styles.scss';
 
@@ -48,8 +49,7 @@ class SignIn extends Component {
             handleChange={this.handleChange}
             required
           />
-
-          <input type="submit" value="Submit form" />
+          <CustomButton type="submit"> Sign In </CustomButton>
         </form>
       </div>
     );

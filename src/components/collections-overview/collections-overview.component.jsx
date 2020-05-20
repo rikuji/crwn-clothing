1226d10da350d5bab5d90a20f92e48b60db3288e
Lucas <lucasbezerra1701@gmail.com>
2020-05-20 08:12:10 -0300

    M10#03

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollecionProps }) => (
+      <CollectionPreview key={id} {...otherCollecionProps} />
+    ))}
+  </div>
+);
+
+const mapToStateProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapToStateProps)(CollectionsOverview);

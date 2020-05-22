7534d5a51a190dca0f12a36c09b65cbe7cee9efe
Lucas <lucasbezerra1701@gmail.com>
2020-05-22 14:22:56 -0300

    FIREBASE

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyBtFJ8gIi0wuFaGLXJ84dEwQNOK-1Ktvdo',
-  authDomain: 'crwn-db-35761.firebaseapp.com',
-  databaseURL: 'https://crwn-db-35761.firebaseio.com',
-  projectId: 'crwn-db-35761',
-  storageBucket: 'crwn-db-35761.appspot.com',
-  messagingSenderId: '1024236264765',
-  appId: '1:1024236264765:web:4b838b62d1c70d49143954',
-  measurementId: 'G-DYP371QJSZ',
-};
+const config = {};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) {
